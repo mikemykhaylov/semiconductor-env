51c8beed41e60973638c831885bae6417a0f47b1
Michael Mykhaylov <mikemikhailov2003@gmail.com>
2023-11-19 14:42:18 -0700

    Remove episode length limit

--- semiconductor_env/__init__.py
+++ semiconductor_env/__init__.py
@@ -4,12 +4,11 @@ Semiconductor manufacturing environment for Gymnasium.
 
 from gymnasium.envs.registration import register
 
-from .env import *
+from .env import *  # noqa: F403
 
 register(
     id="SemiconductorEnv-v0",
     entry_point="semiconductor_env.env:SemiconductorEnv",
-    max_episode_steps=1000,
 )
 
 __version__ = "0.1.0"
